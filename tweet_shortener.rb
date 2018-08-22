dc6218329bb9f01d2d8fae348c051cf6c5091179
Jackie Ha <jha2@wellesley.edu>
2018-08-22 00:29:52 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -46,5 +46,10 @@ def selective_tweet_shortener(tweet)
   end 
 end 
 
-def 
+def shortened_tweet_truncator(tweet)
+  if tweet.length > 140
+    tweet = word_substituter(tweet)
+    tweet[0..135] + "..."
+  end
+end 
   
