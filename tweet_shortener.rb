f14c6aac49c7a3a577d5ec3d7b83a1102504524f
Jackie Ha <jha2@wellesley.edu>
2018-08-22 00:22:08 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,4 +1,30 @@
 # Write your code here.
 
-def dictionary(word)
+def dictionary
+  has = {
+    "Hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+    }
 end
+
+def word_substituter(string)
+  array = string.split(" ")
+  array.each_with_index do |words, index|
+    dictionary.keys.each do |x|
+      if x == words
+        array[index] = dicationary[x]
+      end
+    end
+  end
+  array.join(" ")
+end 
+
+def bulk
